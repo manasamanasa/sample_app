d498068c30f1bc194fa3613127a6a30e96497c08
Samuel Manasse <samuelmanasse@hotmail.fr>
2012-07-12 23:23:58 +0200

    make a basic user (whithout secure password???!)

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.6'
 gem 'bootstrap-sass', '2.0.0'
+gem 'bcrypt-ruby', '3.0.1'
 gem 'therubyracer', :platform => :ruby
 
 group :development, :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,15 @@
 #  created_at :datetime        not null
 #  updated_at :datetime        not null
 #
-
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :name, :email, :password, :password_confirmation
+  
+  before_save { |user| user.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence:   true,
+                    format:     { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  
 end
